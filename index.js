52d3611c1c8f2519f40f37b1a23effa2e6bb4f80
Ajowi254 <ajowi.bery@gmail.com>
2023-06-14 16:47:32 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide= function(){
+    return 2000/100 ;
+}
+
+const square =(x)=>{
+    x => 2*2
+    return  4
+}
+
+const add= function(){
+    //const add = (parameter1, parameter2) => parameter1 + parameter2;
+
+    return (3+4)
+}
